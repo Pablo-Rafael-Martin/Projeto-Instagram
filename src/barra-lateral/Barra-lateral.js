14fce4f0b06d634c1ca92849f7aa8fa69700f510
Pablo-Rafael-Martin <pablo.rafael.cfm@gmail.com>
2024-01-10 20:18:34 -0300

    Botões da barra lateral adicionados.

--- src/barra-lateral/Barra-lateral.js
+++ src/barra-lateral/Barra-lateral.js
@@ -1,6 +1,5 @@
 import './Barra-lateral.css';
 import logo from '../Instagram-Logo.png';
-import Pesquisa from './pesquisa/pesquisa';
 
 function BarraLateral() {
     return(
@@ -8,44 +7,64 @@ function BarraLateral() {
             <ul class='BarraLateral'>
                 <img src={logo} />
                 <li>
-                    <ion-icon name="home"></ion-icon>
-                    <span>Página inicial</span>
+                    <button>
+                        <ion-icon name="home"></ion-icon>
+                        <span>Página inicial</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="search-outline"></ion-icon>
-                    <span>Pesquisa</span>
+                    <button>
+                        <ion-icon name="search-outline"></ion-icon>
+                        <span>Pesquisa</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="compass-outline"></ion-icon>
-                    <span>Explorar</span>
+                    <button>
+                        <ion-icon name="compass-outline"></ion-icon>
+                        <span>Explorar</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="film-outline"></ion-icon>
-                    <span>Reels</span>
+                    <button>
+                        <ion-icon name="film-outline"></ion-icon>
+                        <span>Reels</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                    <span>Mensagens</span>
+                    <button>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                        <span>Mensagens</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <span>Notificações</span>
+                    <button>
+                        <ion-icon name="heart-outline"></ion-icon>
+                        <span>Notificações</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="add-circle-outline"></ion-icon>
-                    <span>Criar</span>
+                    <button>
+                        <ion-icon name="add-circle-outline"></ion-icon>
+                        <span>Criar</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="person-circle-outline"></ion-icon>
-                    <span>Perfil</span>
+                    <button>
+                        <ion-icon name="person-circle-outline"></ion-icon>
+                        <span>Perfil</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="at-outline"></ion-icon>
-                    <span>Threads</span>
+                    <button>
+                        <ion-icon name="at-outline"></ion-icon>
+                        <span>Threads</span>
+                    </button>
                 </li>
                 <li>
-                    <ion-icon name="reorder-three-outline"></ion-icon>
-                    <span>Mais</span>
+                    <button>
+                        <ion-icon name="reorder-three-outline"></ion-icon>
+                        <span>Mais</span>
+                    </button>
                 </li>
             </ul>
         </div>

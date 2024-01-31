79c558271978723346c28ceff91741ef77010347
Pablo-Rafael-Martin <pablo.rafael.cfm@gmail.com>
2024-01-31 12:00:40 -0300

    Barra inferior finalizada

--- src/barra-inferior/barra-inferior.js
+++ src/barra-inferior/barra-inferior.js
@@ -1,9 +1,45 @@
 import './style.css';
+import fotoDePerfil from '../foto-de-perfil.jpg';
 
 function BarraInferior() {
     return (
         <div id='BarraInferior'>
-            
+            <nav>
+                <ul>
+                    <li>
+                        <button>
+                            <i class="fa-solid fa-house"></i>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <i class="fa-regular fa-compass"></i>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <i class="fa-solid fa-film"></i>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <i class="fa-regular fa-square-plus"></i>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <i class="fa-regular fa-paper-plane"></i>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <img src={fotoDePerfil}/>
+                        </button>
+                    </li>
+
+                    
+                </ul>
+            </nav>
         </div>
     );
 }
